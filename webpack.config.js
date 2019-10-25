c1236e3b53f4d63484fbdf7a5c6160cef6baeaaa
Alex <oleksandr.khmil2@gmail.com>
2019-10-25 13:24:55 +0300

    html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs')
+
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map(item => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false,
+    })
+  })
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/html/pages')
 
 module.exports = {
   entry: [
@@ -42,6 +60,11 @@ module.exports = {
           ]
         })
       },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/html/components'),
+        use: ['raw-loader']
+      },
     ]
   },
   plugins: [
@@ -49,5 +72,5 @@ module.exports = {
       filename: './css/style.bundle.css',
       allChunks: true,
     }),
-  ]
+  ].concat(htmlPlugins)
 };
