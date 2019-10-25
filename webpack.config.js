22afdcd3c50901760f8ec46eb4d1d8b36b44e90b
Alex <oleksandr.khmil2@gmail.com>
2019-10-25 13:12:54 +0300

    scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: [
     './src/js/index.js',
+    './src/scss/style.scss'
   ],
   output: {
     filename: './js/bundle.js'
@@ -19,8 +21,33 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                //minimize: true,
+                url: false
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
     ]
   },
   plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
   ]
 };
